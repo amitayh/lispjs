1e42ba3e78a0887f5ea4bc3234d42cc07f57e464
Amitay Horwitz <amitayh@wix.com>
2015-08-20 18:38:16 +0300

    Basic implementation works

--- specs/lisp-spec.js
+++ specs/lisp-spec.js
@@ -1,17 +1,158 @@
-var expect = require('expect');
+var assert = require('assert');
 var lisp = require('../src/lisp');
 
 describe('lispjs', function () {
 
-  describe('evaluate()', function () {
+  function identity(arg) {
+    return arg;
+  }
 
+  function constantly(value) {
+    return function () {
+      return value;
+    };
+  }
+
+  describe('simple evaluation', function () {
     it('should evaluate simple values as themselves', function () {
       var expr1 = 1, expr2 = 'foo';
       var env = {};
-      expect(lisp.evaluate(expr1, env)).toEqual([expr1, env]);
-      expect(lisp.evaluate(expr2, env)).toEqual([expr2, env]);
+      assert.deepEqual(lisp.evaluate(expr1, env), [expr1, env]);
+      assert.deepEqual(lisp.evaluate(expr2, env), [expr2, env]);
     });
 
+    it('should evaluate bound symbols to their values', function () {
+      var expr = 'foo';
+      var env = {foo: 'bar'};
+      assert.equal(lisp.getResult(expr, env), 'bar');
+    });
+  });
+
+  describe('function invocation', function () {
+    it('should invoke function without arguments', function () {
+      var expr = ['foo'];
+      var env = {foo: constantly('bar')};
+      assert.equal(lisp.getResult(expr, env), 'bar');
+    });
+
+    it('should invoke function with arguments', function () {
+      var expr = ['foo', 'bar'];
+      var env = {foo: identity};
+      assert.equal(lisp.getResult(expr, env), 'bar');
+    });
+
+    it('should evaluate function arguments before invoking a function', function () {
+      var expr = ['foo', 'bar'];
+      var env = {foo: identity, bar: 'baz'};
+      assert.equal(lisp.getResult(expr, env), 'baz');
+    });
+  });
+
+  describe('define', function () {
+    it('should bind values in environment when calling define', function () {
+      var expr = ['define', 'foo', 'bar'];
+      assert.deepEqual(lisp.evaluate(expr, {}), [null, {foo: 'bar'}]);
+    });
+
+    it('should keep the original environment unchanged', function () {
+      var env = {};
+      var expr = ['define', 'foo', 'bar'];
+      lisp.evaluate(expr, env);
+      assert.equal(env.foo, undefined);
+    });
+
+    it('should evaluate values when calling define', function () {
+      var expr = ['define', 'foo', 'bar'];
+      var env = {bar: 'baz'};
+      var newEnv = lisp.evaluate(expr, env)[1];
+      assert.equal(newEnv.foo, 'baz');
+    });
+  });
+
+  describe('if', function () {
+    it('should allow simple branching', function () {
+      var env = {};
+      assert.equal(lisp.getResult(['if', true, 'foo', 'bar'], env), 'foo');
+      assert.equal(lisp.getResult(['if', false, 'foo', 'bar'], env), 'bar');
+    });
+
+    it('should evaluate condition before branching', function () {
+      assert.equal(lisp.getResult(['if', 'cond', 'foo', 'bar'], {cond: true}), 'foo');
+      assert.equal(lisp.getResult(['if', 'cond', 'foo', 'bar'], {cond: false}), 'bar');
+    });
+
+    it('should evaluate branches', function () {
+      var env = {then: 'foo', otherwise: 'bar'};
+      assert.equal(lisp.getResult(['if', true, 'then', 'otherwise'], env), 'foo');
+      assert.equal(lisp.getResult(['if', false, 'then', 'otherwise'], env), 'bar');
+    });
+  });
+
+  it('should evaluate multiple blocks of code', function () {
+    var prog = [
+      ['define', 'foo', 'bar'],
+      'foo'
+    ];
+    assert.equal(lisp.getResultMulti(prog, {}), 'bar');
+  });
+
+  describe('closures', function () {
+    it('should define simple function without arguments', function () {
+      var expr = ['define', 'foo', ['func', [], 'bar']];
+      var result = lisp.evaluate(expr, {});
+      assert.equal(lisp.getResult(['foo'], result[1]), 'bar');
+    });
+
+    it('should bind arguments when calling a function', function () {
+      var prog = [
+        ['define', 'foo', ['func', ['arg'], 'arg']],
+        ['foo', 'bar']
+      ];
+      assert.equal(lisp.getResultMulti(prog, {}), 'bar');
+    });
+
+    it('should support lexical scope', function () {
+      var prog = [
+        ['define', 'foo', 'bar'],
+        ['define', 'bar', ['func', [], 'foo']],
+        ['bar']
+      ];
+      assert.equal(lisp.getResultMulti(prog, {}), 'bar');
+    });
+
+    it('should give local scope higher priority', function () {
+      var prog = [
+        ['define', 'foo', 'bar'],
+        ['define', 'bar', ['func', ['foo'], 'foo']],
+        ['bar', 'baz']
+      ];
+      assert.equal(lisp.getResultMulti(prog, {}), 'baz');
+    });
+
+    it('should allow recursion', function () {
+      var env = {
+        '<': function (a, b) { return a < b; },
+        '+': function (a, b) { return a + b; },
+        '-': function (a, b) { return a - b; }
+      };
+      var prog =
+        ['define', 'fib',
+          ['func', ['n'],
+            ['if', ['<', 'n', 2],
+              1,
+              ['+',
+                ['fib', ['-', 'n', 1]],
+                ['fib', ['-', 'n', 2]]]]]];
+
+      var fibEnv = lisp.evaluate(prog, env)[1];
+
+      assert.equal(lisp.getResult(['fib', 0], fibEnv), 1);
+      assert.equal(lisp.getResult(['fib', 1], fibEnv), 1);
+      assert.equal(lisp.getResult(['fib', 2], fibEnv), 2);
+      assert.equal(lisp.getResult(['fib', 3], fibEnv), 3);
+      assert.equal(lisp.getResult(['fib', 4], fibEnv), 5);
+      assert.equal(lisp.getResult(['fib', 5], fibEnv), 8);
+    });
   });
 
 });
--- src/lisp.js
+++ src/lisp.js
@@ -1,7 +1,93 @@
 function evaluate(expr, env) {
+  if (isBound(expr, env)) {
+    return [env[expr], env];
+  }
+  if (Array.isArray(expr)) {
+    var func = expr[0];
+    if (func === 'define') {
+      return define(expr, env);
+    }
+    if (func === 'func') {
+      return closure(expr[1], expr[2], env);
+    }
+    if (func === 'if') {
+      return branch(expr[1], expr[2], expr[3], env);
+    }
+    return invoke(func, expr.slice(1), env);
+  }
   return [expr, env];
 }
 
+function getResult(expr, env) {
+  return evaluate(expr, env)[0];
+}
+
+function getResultMulti(prog, env) {
+  var result = [null, env];
+  prog.forEach(function (expr) {
+    result = evaluate(expr, result[1]);
+  });
+  return result[0];
+}
+
+function isBound(expr, env) {
+  return (typeof expr === 'string' && env[expr] !== undefined);
+}
+
+function define(expr, env) {
+  var name = expr[1];
+  var newEnv = copy(env);
+  newEnv[name] = getResult(expr[2], env);
+  return [null, newEnv];
+}
+
+function closure(argsList, body, env) {
+  var result = function () {
+    var closureEnv = copy(this);
+    merge(closureEnv, env);
+    for (var i = 0; i < arguments.length; i++) {
+      closureEnv[argsList[i]] = arguments[i];
+    }
+    return getResult(body, closureEnv);
+  };
+  return [result, env];
+}
+
+function branch(cond, then, otherwise, env) {
+  var result = getResult(cond, env) ?
+    getResult(then, env) :
+    getResult(otherwise, env);
+  return [result, env];
+}
+
+function invoke(func, args, env) {
+  var evaluatedArgs = args.map(function (arg) {
+    return getResult(arg, env);
+  });
+  var result = env[func].apply(env, evaluatedArgs);
+  return [result, env];
+}
+
+function copy(obj) {
+  var result = {};
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
+
+function merge(target, source) {
+  for (var key in source) {
+    if (source.hasOwnProperty(key)) {
+      target[key] = source[key];
+    }
+  }
+}
+
 module.exports = {
-  evaluate: evaluate
+  evaluate: evaluate,
+  getResult: getResult,
+  getResultMulti: getResultMulti
 };
