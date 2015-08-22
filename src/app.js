62f23cdd51ed0c3f482f83d75837c8ad7015f9f0
Amitay Horwitz <amitayh@wix.com>
2015-08-22 23:04:50 +0300

    Moved to bootstrap CSS

--- src/app.js
+++ src/app.js
@@ -6,34 +6,74 @@ var clearKeyCode = 76; // The letter L
 
 var ResultItem = React.createClass({
   render: function () {
-    var result = this.props.result;
     return (
       <li className="result-item">
-        <pre className="result-expr">{result.expr}</pre>
-        <pre>{JSON.stringify(result.value)}</pre>
+        <pre className="result-expr">{this.props.result.expr}</pre>
+        <pre className={this.getResultClass()}>{this.getResultValue()}</pre>
       </li>
     );
+  },
+  getResultClass: function () {
+    return this.props.result.error ? 'alert-danger' : '';
+  },
+  getResultValue: function () {
+    var result = this.props.result, error = result.error;
+    return error ? error.toString() : JSON.stringify(result.value);
   }
 });
 
 var InputBox = React.createClass({
+  defaultExpr: '["+", 1, 2]',
   getInitialState: function () {
-    return {expr: '["+", 1, 2]'}
+    return {
+      expr: this.defaultExpr,
+      parsed: JSON.parse(this.defaultExpr),
+      exprInvalid: false
+    }
   },
   render: function () {
     return (
-      <div className="pure-form">
-        <textarea className="pure-input-1" rows="5" cols="80" value={this.state.expr} onChange={this.onExprChange} onKeyDown={this.onKeyDown} />
-        <p>
-          <small className="notes">Ctrl+Enter to evaluate, Ctrl+L to clear</small>
-          <button className="pure-button" onClick={this.onEvalClick}>Evaluate expression</button>{' '}
-          <button className="pure-button" onClick={this.onClearClick}>Clear results</button>
-        </p>
+      <div>
+        <div className={this.getTextAreaClass()}>
+          <textarea className="form-control" rows="5" cols="80" value={this.state.expr} onChange={this.onExprChange} onKeyDown={this.onKeyDown} />
+        </div>
+        <div className="form-group">
+          <small className="notes pull-right">Ctrl+Enter to evaluate, Ctrl+L to clear</small>
+          <div className="btn-group">
+            <button className="btn btn-primary" onClick={this.onEvalClick} disabled={this.getEvalDisabled()}>
+              <span className="glyphicon glyphicon-play" aria-hidden="true"></span>{' '}
+              Evaluate expression
+            </button>{' '}
+            <button className="btn btn-default" onClick={this.onClearClick} disabled={this.props.disableClear}>
+              <span className="glyphicon glyphicon-ban-circle" aria-hidden="true"></span>{' '}
+              Clear results
+            </button>
+          </div>
+        </div>
       </div>
     );
   },
+  getTextAreaClass: function () {
+    var classes = ['form-group'];
+    if (this.state.exprInvalid) {
+      classes.push('has-error');
+    }
+    return classes.join(' ');
+  },
+  getEvalDisabled: function () {
+    var state = this.state;
+    return (state.exprInvalid || state.expr === '');
+  },
   onExprChange: function (e) {
-    this.setState({expr: e.target.value});
+    var expr = e.target.value, exprInvalid = false, parsed;
+    if (expr !== '') {
+      try {
+        parsed = JSON.parse(expr);
+      } catch (e) {
+        exprInvalid = true;
+      }
+    }
+    this.setState({expr: expr, parsed: parsed, exprInvalid: exprInvalid});
   },
   onKeyDown: function (e) {
     if (e.ctrlKey && (e.keyCode == enterKeyCode || e.keyCode == clearKeyCode)) {
@@ -78,18 +118,16 @@ var Repl = React.createClass({
   getInitialState: function () {
     return {
       env: lisp.defaultEnv,
-      results: [],
-      error: null
+      results: []
     };
   },
   render: function () {
     return (
       <div>
-        <ul>
+        <ul className="list-unstyled">
           {this.getResults()}
-          <li><InputBox onEvaluate={this.onEvaluate} onClear={this.onClear} onError={this.onError} /></li>
+          <li><InputBox onEvaluate={this.onEvaluate} onClear={this.onClear} disableClear={this.getDisableClear()} /></li>
         </ul>
-        <ErrorView error={this.state.error} />
       </div>
     );
   },
@@ -98,24 +136,24 @@ var Repl = React.createClass({
       return <ResultItem key={index} result={result} />;
     });
   },
+  getDisableClear: function () {
+    return (this.state.results.length === 0);
+  },
   onEvaluate: function (expr, parsed) {
+    var env = this.state.env, value, error;
     try {
       var evaluated = lisp.evaluate(parsed, this.state.env);
-      var newResult = {expr: expr, value: evaluated[0]};
-      this.setState({
-        results: this.state.results.concat([newResult]),
-        env: evaluated[1],
-        error: null
-      });
+      value = evaluated[0];
+      env = evaluated[1];
     } catch (e) {
-      this.onError(e);
+      error = e;
     }
+    var newResult = {expr: expr, value: value, error: error};
+    var results = this.state.results.concat([newResult]);
+    this.setState({results: results, env: env});
   },
   onClear: function () {
-    this.setState({results: [], error: null});
-  },
-  onError: function (error) {
-    this.setState({error: error});
+    this.setState({results: []});
   }
 });
 
